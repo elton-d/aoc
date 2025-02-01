830e94a2d4bb87095f1e270c7140b96a46e9cfb6
Elton DSouza <eltoncdsouza@gmail.com>
2025-02-01 02:51:27 -0800

    day 2

--- 2024/day-02/solution.go
+++ 2024/day-02/solution.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/elton-d/aoc/util"
 )
@@ -9,5 +13,72 @@ import (
 func main() {
 	input := util.GetInputStr("https://adventofcode.com/2024/day/2/input")
 
-	fmt.Printf("Input:\n %s", input)
+	reports, err := getReports(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(numSafe(reports, safe))
+	fmt.Println(numSafe(reports, safeWithRemovals))
+
+}
+
+func getReports(input string) ([][]int, error) {
+	var reports = [][]int{}
+	for _, l := range strings.Split(input, "\n") {
+		var report = []int{}
+		for _, s := range strings.Split(l, " ") {
+			if n, err := strconv.Atoi(s); err != nil {
+				return nil, err
+			} else {
+				report = append(report, n)
+			}
+		}
+		reports = append(reports, report)
+	}
+	return reports, nil
+}
+
+func safe(report []int) bool {
+	increasing := true
+	decreasing := true
+	for i, val := range report[1:] {
+		diff := val - report[i]
+		if diff == 0 || math.Abs(float64(diff)) > 3 {
+			return false
+		}
+		decreasing = decreasing && (diff < 0)
+		increasing = increasing && (diff > 0)
+
+	}
+
+	return increasing || decreasing
+}
+
+func safeWithRemovals(report []int) bool {
+	if safe(report) {
+		return true
+	}
+
+	for i := range report {
+		var newSlice []int
+		if i == len(report)-1 {
+			newSlice = report[:i]
+		} else {
+			newSlice = slices.Concat(report[:i], report[i+1:])
+		}
+		if safe(newSlice) {
+			return true
+		}
+	}
+	return false
+}
+
+func numSafe(reports [][]int, checker func([]int) bool) int {
+	var count int
+	for _, r := range reports {
+		if checker(r) {
+			count++
+		}
+	}
+	return count
 }
--- 2024/day-02/solution_test.go
+++ 2024/day-02/solution_test.go
@@ -2,6 +2,24 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
+func TestSafe(t *testing.T) {
+	rep, err := getReports(`7 6 4 2 1`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !safe(rep[0]) {
+		t.Errorf("want safe")
+	}
+
+}
+
+func TestSafeWithRemovals(t *testing.T) {
+	rep, err := getReports(`9 7 6 2 1`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if safeWithRemovals(rep[0]) {
+		t.Errorf("want unsafe")
+	}
 
 }
