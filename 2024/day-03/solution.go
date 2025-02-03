c32588294b4961b1cd343fbb989a0d650028cf59
Elton DSouza <eltoncdsouza@gmail.com>
2025-02-03 01:17:12 -0800

    day 3

--- 2024/day-03/solution.go
+++ 2024/day-03/solution.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/elton-d/aoc/util"
 )
@@ -9,5 +13,93 @@ import (
 func main() {
 	input := util.GetInputStr("https://adventofcode.com/2024/day/3/input")
 
-	fmt.Printf("Input:\n %s", input)
+	s, err := findSum(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Part 1: %d\n", s)
+
+	s, err = findSum2(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Part 2: %d\n", s)
+}
+
+func findMatches(instrs string) ([]string, error) {
+	matches := []string{}
+	re, err := regexp.Compile(`mul\(\d{1,3}\,\d{1,3}\)`)
+	if err != nil {
+		return nil, err
+	}
+	for _, l := range strings.Split(instrs, "\n") {
+		matches = slices.Concat(matches, re.FindAllString(l, -1))
+	}
+	return matches, nil
+}
+
+func findMatches2(instrs string) ([]string, error) {
+	matches := []string{}
+	re, err := regexp.Compile(`(mul\(\d{1,3}\,\d{1,3}\))|(do\(\))|(don\'t\(\))`)
+	if err != nil {
+		return nil, err
+	}
+	for _, l := range strings.Split(instrs, "\n") {
+		matches = slices.Concat(matches, re.FindAllString(l, -1))
+	}
+	return matches, nil
+}
+
+func findSum2(instrs string) (int, error) {
+	sum := 0
+	keepCount := true
+	matches, err := findMatches2(instrs)
+	if err != nil {
+		return -1, err
+	}
+	re := regexp.MustCompile(`\d+`)
+
+	for _, m := range matches {
+		if m == "do()" {
+			keepCount = true
+			continue
+		} else if m == "don't()" {
+			keepCount = false
+			continue
+		}
+		res := 1
+		if keepCount {
+			for _, n := range re.FindAllString(m, -1) {
+				if i, err := strconv.Atoi(n); err != nil {
+					return -1, err
+				} else {
+					res *= i
+				}
+			}
+			sum += res
+		}
+	}
+	return sum, nil
+}
+
+func findSum(instrs string) (int, error) {
+	sum := 0
+	matches, err := findMatches(instrs)
+	if err != nil {
+		return -1, err
+	}
+	re := regexp.MustCompile(`\d+`)
+
+	for _, m := range matches {
+		res := 1
+		for _, n := range re.FindAllString(m, -1) {
+			if i, err := strconv.Atoi(n); err != nil {
+				return -1, err
+			} else {
+				res *= i
+			}
+		}
+		sum += res
+	}
+	return sum, nil
 }
--- 2024/day-03/solution_test.go
+++ 2024/day-03/solution_test.go
@@ -1,7 +1,56 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestMain(t *testing.T) {
+	"github.com/google/go-cmp/cmp"
+)
 
+func TestFindMatches(t *testing.T) {
+	tests := []struct {
+		s    string
+		want []string
+	}{
+		{
+			s:    "mul(12,33)}I{SDFISDF{SPDF{Pmul(12,34)",
+			want: []string{"mul(12,33)", "mul(12,34)"},
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("%s-%d", t.Name(), i), func(t *testing.T) {
+			got, err := findMatches(tc.s)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !cmp.Equal(got, tc.want) {
+				t.Errorf("got: %q, want: %q", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestFindMatches2(t *testing.T) {
+	tests := []struct {
+		s    string
+		want []string
+	}{
+		{
+			s:    "mul(12,33)don't()}I{SDFISDF{SPDF{Pmul(12,34)do()",
+			want: []string{"mul(12,33)", "don't()", "mul(12,34)", "do()"},
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("%s-%d", t.Name(), i), func(t *testing.T) {
+			got, err := findMatches2(tc.s)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !cmp.Equal(got, tc.want) {
+				t.Errorf("got: %q, want: %q", got, tc.want)
+			}
+		})
+	}
 }
