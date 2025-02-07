18e3f2e03d5a5b047a3d94d75462a3d550d8c6f8
Elton DSouza <eltoncdsouza@gmail.com>
2025-02-06 20:18:33 -0800

    Day 7 solution

--- 2024/day-07/solution.go
+++ 2024/day-07/solution.go
@@ -2,12 +2,109 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/elton-d/aoc/util"
 )
 
+type equation struct {
+	target   int
+	operands []int
+}
+
+type operation struct {
+	applyFn func(x, y int) int
+}
+
+func (o *operation) apply(x, y int) int {
+	return o.applyFn(x, y)
+}
+
+var add = &operation{applyFn: func(x, y int) int { return x + y }}
+var mul = &operation{applyFn: func(x, y int) int { return x * y }}
+var concat = &operation{applyFn: func(x, y int) int {
+	res, err := strconv.Atoi(fmt.Sprintf("%d%d", x, y))
+	if err != nil {
+		panic(err)
+	}
+	return res
+}}
+
+func (e *equation) String() string {
+	return fmt.Sprintf("{target: %d; operands: %v}", e.target, e.operands)
+}
+
+func parseInput(input string) ([]*equation, error) {
+	lines := strings.Split(input, "\n")
+	var err error
+	equations := make([]*equation, len(lines))
+	for i, line := range lines {
+		eq := &equation{}
+		parts := strings.Split(line, ":")
+		eq.target, err = strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, err
+		}
+		nums := strings.Split(strings.TrimSpace(parts[1]), " ")
+		eq.operands = make([]int, len(nums))
+		for j, s := range nums {
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				return nil, err
+			}
+			eq.operands[j] = n
+		}
+		equations[i] = eq
+	}
+	return equations, nil
+}
+
+func checkRec(eq *equation, curr int, idx int, op *operation, validOps []*operation) bool {
+	if idx == len(eq.operands) {
+		return curr == eq.target
+	}
+
+	val := op.apply(curr, eq.operands[idx])
+	for _, o := range validOps {
+		if checkRec(eq, val, idx+1, o, validOps) {
+			return true
+		}
+	}
+	return false
+}
+
+func part1(eqs []*equation) int {
+	var sum int
+	validOps := []*operation{add, mul}
+
+	for _, eq := range eqs {
+		if checkRec(eq, 0, 0, add, validOps) || checkRec(eq, 1, 0, mul, validOps) {
+			sum += eq.target
+		}
+	}
+	return sum
+}
+
+func part2(eqs []*equation) int {
+	var sum int
+	validOps := []*operation{add, mul, concat}
+
+	for _, eq := range eqs {
+		if checkRec(eq, 0, 0, add, validOps) || checkRec(eq, 1, 0, mul, validOps) || checkRec(eq, 0, 0, concat, validOps) {
+			sum += eq.target
+		}
+	}
+	return sum
+}
+
 func main() {
 	input := util.GetInputStr("https://adventofcode.com/2024/day/7/input")
 
-	fmt.Printf("Input:\n %s", input)
+	eqs, err := parseInput(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Part 1: %d\n", part1(eqs))
+	fmt.Printf("Part 2: %d\n", part2(eqs))
 }
--- 2024/day-07/solution_test.go
+++ 2024/day-07/solution_test.go
@@ -2,6 +2,50 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
+func TestPart1(t *testing.T) {
+	input := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+	eqs, err := parseInput(input)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	want := 3749
+
+	got := part1(eqs)
+
+	if got != want {
+		t.Errorf("got: %d, want: %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+	eqs, err := parseInput(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 11387
+
+	got := part2(eqs)
+
+	if got != want {
+		t.Errorf("got: %d, want: %d", got, want)
+	}
 }
