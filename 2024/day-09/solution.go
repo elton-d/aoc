1f29544f300dae5e4a7fa6eeb9797f92d2e4d2ff
Elton DSouza <eltoncdsouza@gmail.com>
2025-02-25 20:04:21 -0800

    day 9 solution

--- 2024/day-09/solution.go
+++ 2024/day-09/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/elton-d/aoc/util"
 )
@@ -9,5 +10,138 @@ import (
 func main() {
 	input := util.GetInputStr("https://adventofcode.com/2024/day/9/input")
 
-	fmt.Printf("Input:\n %s", input)
+	fmt.Printf("Part 1: %d\n", checksum(compact(expand(input))))
+	fmt.Printf("Part 2: %d\n", checksum(compact2(expand(input))))
+}
+
+type freeSpace struct {
+	idx    int
+	blocks int
+}
+
+func expand(input string) []string {
+	var out []string
+	var id int
+
+	for i, c := range input {
+		times, err := strconv.Atoi(string(c))
+		if err != nil {
+			panic(err)
+		}
+		if i%2 == 0 {
+			for range times {
+				out = append(out, fmt.Sprintf("%d", id))
+			}
+			id++
+		} else {
+			for range times {
+				out = append(out, ".")
+			}
+		}
+	}
+	return out
+}
+
+func compact(input []string) []string {
+	var l, r int
+	output := make([]string, len(input))
+	copy(output, input)
+	r = len(input) - 1
+
+	for l < r {
+		if output[l] != "." {
+			l++
+			continue
+		}
+		if output[r] == "." {
+			r--
+			continue
+		}
+		output[l] = output[r]
+		output[r] = "."
+		l++
+		r--
+	}
+	return output
+}
+
+func compact2(input []string) []string {
+	var freeSpaces []freeSpace
+
+	var k int
+	for k = 0; k < len(input); {
+		if input[k] == "." {
+			start := k
+			end := k
+			for end = k; end < len(input); end++ {
+				if input[end] != "." {
+					end--
+					break
+				}
+			}
+			freeSpaces = append(freeSpaces, freeSpace{idx: start, blocks: end - start + 1})
+			k = end + 1
+		} else {
+			k++
+		}
+	}
+	output := make([]string, len(input))
+	copy(output, input)
+
+	var srcEnd, srcStart int
+
+	for r := len(input) - 1; r >= 0; {
+		if input[r] != "." {
+			srcEnd = r
+			srcStart = r
+			fileID := input[r]
+
+			for i := r; i >= 0; i-- {
+				if input[i] == fileID {
+					srcStart = i
+				} else {
+					break
+				}
+			}
+
+			fileSize := srcEnd - srcStart + 1
+			for i := 0; i < len(freeSpaces); i++ {
+				if freeSpaces[i].idx >= srcStart {
+					break
+				}
+				if freeSpaces[i].blocks >= fileSize {
+					dst := freeSpaces[i].idx
+					for j := srcStart; j <= srcEnd; j++ {
+						output[dst] = output[j]
+						output[j] = "."
+						dst++
+					}
+					freeSpaces[i].blocks = freeSpaces[i].blocks - fileSize
+					freeSpaces[i].idx = freeSpaces[i].idx + fileSize
+					break
+				}
+			}
+			r = srcStart - 1
+		} else {
+			r--
+		}
+	}
+
+	return output
+}
+
+func checksum(input []string) int {
+	var sum int
+	for i, id := range input {
+		if id == "." {
+			continue
+		}
+
+		n, err := strconv.Atoi(id)
+		if err != nil {
+			panic(err)
+		}
+		sum += i * n
+	}
+	return sum
 }
--- 2024/day-09/solution_test.go
+++ 2024/day-09/solution_test.go
@@ -1,7 +1,99 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
-func TestMain(t *testing.T) {
+func TestExpand(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "2333133121414131402",
+			want:  "00...111...2...333.44.5555.6666.777.888899",
+		},
+		{
+			input: "12345",
+			want:  "0..111....22222",
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			got := strings.Join(expand(tc.input), "")
+			if got != tc.want {
+				t.Errorf("got: %s, want: %s", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestCompact2(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "00...111...2...333.44.5555.6666.777.888899",
+			want:  "00992111777.44.333....5555.6666.....8888..",
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			got := strings.Join(compact2(strings.Split(tc.input, "")), "")
+			if got != tc.want {
+				t.Errorf("got: %s, want: %s", got, tc.want)
+			}
+		})
+	}
+}
+func TestCompact(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "00...111...2...333.44.5555.6666.777.888899",
+			want:  "0099811188827773336446555566..............",
+		},
+		{
+
+			input: "0..111....22222",
+			want:  "022111222......",
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			got := strings.Join(compact(strings.Split(tc.input, "")), "")
+			if got != tc.want {
+				t.Errorf("got: %s, want: %s", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestChecksum(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: "2333133121414131402",
+			want:  1928,
+		},
+	}
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			got := checksum(compact(expand(tc.input)))
+			if got != tc.want {
+				t.Errorf("got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
 
 }
